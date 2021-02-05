aa22cd1cb5017975c5b78206aba2a9421713c2d3
Ted <whatisontop@gmail.com>
2021-02-04 21:10:30 -0500

    from new user

--- a.b.git.test/src/a/b/git/test/GitTest.java
+++ a.b.git.test/src/a/b/git/test/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("Hello Git world!");
 		System.out.println("Add new message2.");
+		System.out.println("Add new message3.");
 
 	}
 
