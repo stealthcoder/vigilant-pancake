8189643eff41a335d2613ddf99f222d3f99752ee
a$tig <stealthcoder@users.noreply.github.com>
2021-02-26 14:22:48 -0500

    Update GitTest.java

--- a.b.git.test/src/a/b/git/test/GitTest.java
+++ a.b.git.test/src/a/b/git/test/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git world!");
+		System.out.println("Add new message1.");
 		System.out.println("Add new message2.");
 		System.out.println("Add new message3.");
 		System.out.println("Add new message4.");
